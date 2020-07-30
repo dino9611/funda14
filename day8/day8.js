28b8e9b056a3417c40b45273abdee48d5e0c9366
dino9611 <45329868+dino9611@users.noreply.github.com>
2020-07-30 11:55:09 +0700

    ddad

--- day8/day8.js
+++ day8/day8.js
@@ -1,10 +1,4 @@
-var obj={
-    head:1,
-    eyes:2,
-    hands:2,
-    ears:2,
-    Mouth:1
-}
+var obj={head:1,eyes:2,hands:2,ears:2,Mouth:1}
 var obj1={
     kaki:2
 }
@@ -25,21 +19,49 @@ console.log(bebas1)
 
 var newfunc= input => `manusia punya ${input.head} kepala dan ${input.eyes} mata `
 
-var topostif=(input)=>{
+var topostif=(cb,input)=>{
     if(input<0){
-        return input*-1
+        return cb(input)
     }
     return input
 }
 
-var angka=(a)=>a*2
-// var angka2=(a)=>a*3
+var angka=(cba,a)=>cba(a)*2
+var angka2=(a)=>a*3
 
 var keliling=(cb,p,l)=>{
     return cb(p)+cb(l)//angka(4)+angka(5)
 }
 
-console.log(keliling(angka,4,5))
+// var kelilingper=(p,l)=>new Promise((resolve,reject)=>{
+//     var hasil=keliling((a)=>{
+//         return topostif((b)=>{
+//             return b*-1
+//         },a)*2
+//     },p,l)
+//     // console.log(hasil)
+//     if(hasil){
+//         resolve(hasil)
+//     }
+// })
+
+// kelilingper(-3,-4).then((res)=>{
+//     console.log(res)
+// })
+console.log('aaa')
+
+// keliling(-3,-5).then((res)=>{
+//     res
+// })
+// var hasil=keliling((a)=>{
+//     return topostif((b)=>{
+//         return b*-1
+//     },a)*2
+// },-4,-5)
+
+// console.log(hasil,'ddd')
+
+
 
 
 
@@ -49,8 +71,8 @@ var filteraar=newarr.filter((val,index)=>  val%2===0 )
 const filteralala=(arr,cb)=>{
     
 }
-console.log(filteraar)
-console.log(filteralala(newarr,(val)=> val%2===0))
+// console.log(filteraar)
+// console.log(filteralala(newarr,(val)=> val%2===0))
 
 var arrbaru=newarr.map((terserah,indexterserah)=>{
     if(indexterserah%2){
@@ -75,4 +97,9 @@ var arrbaru=newarr.map((terserah,indexterserah)=>{
 
 
 // console.log(newfunc({head:3,eyes:10}))
-// console.log(newfunc(obj))
+// console.log(newfunc(obj))//{head:1,eyes:2,hands:2,ears:2,Mouth:1}
+//f(x)=x+3
+//a=4
+//b=3
+//f(a)=7
+//f(b)=6
