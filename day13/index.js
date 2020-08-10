e757f5eafb56c2730dc246a822734a86da32e867
dino9611 <dinotestes12@gmail.com>
2020-08-10 11:04:39 +0700

    dsad

---
+++ day13/index.js
@@ -0,0 +1,27 @@
+//harga awal 10000 -12000 -14400-17280
+//tiap menit naik 20%
+//tiap kelipatan 4 naiknya 10%
+// if,96, be, 25 ,go ,715, up, 2116
+//harganya diatas 30 juta return 'barang sudah dibeli'
+const lelang=(menit)=>{
+    var awal=10000
+    var hitunganmenit=1
+    do {
+        if(hitunganmenit%4==0){
+            awal+=Math.ceil(awal*0.1)
+        }else{
+            awal+=Math.ceil(awal*0.2)
+        }
+        hitunganmenit ++
+    } while (hitunganmenit<=menit);
+
+    // console.log(awal)
+    return awal >30000000?'barang sudah dibeli':awal
+}
+console.log(lelang(1))
+
+console.log(lelang(2))
+
+var harihari=['Minggu','Senin','Selasa','Rabu','Kamis','Jumat','Sabtu']
+
+console.log(harihari.findIndex((val)=>val==='Senin'))
